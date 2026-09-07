5faa56bc4a72d0fd557d74feb45d9254b7662b1e
Hani Abdelrahman <hanyouf37@gmail.com>
2026-09-07 17:40:24 +0300

    impelement pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -7,7 +7,6 @@ exports.getAllTours = async (req, res) => {
   try {
     // BUILD QUERY
     // 1A) Filtering
-    console.log(req.query);
     const queryObj = { ...req.query };
     const excludedFields = ["page", "sort", "limit", "fields"];
     excludedFields.forEach((el) => delete queryObj[el]);
@@ -24,7 +23,26 @@ exports.getAllTours = async (req, res) => {
     } else {
       query = query.sort("-createdAt");
     }
+    // 3) Field Limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
 
+    // 4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error("This page does not exist");
+    }
+    //execute query
     const tours = await query;
     //SEND RESPONSE
     res.status(200).json({
